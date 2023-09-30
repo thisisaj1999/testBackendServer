65bd5195707a8494656ce0177051b0e207a7d3ef
thisisaj1999 <atuljoshi94170@gmail.com>
2023-09-30 11:40:11 +0400

    [FIX]:code fixed

--- README.md
+++
@@ -1,5 +0,0 @@
-# CRUD-Project
-
-This project takes the data from the user and stores it in DB as well as it also sends the welcome mail to the new user.
-
-click <a href="https://crud-project22.herokuapp.com/">here<a/> to open project.
--- app.js
+++ app.js
@@ -1,107 +1,28 @@
 const express = require('express');
-require('./db/connect');
-const cors = require('cors');
-
-require('dotenv').config();
-const User = require('./models/userData');
 const https = require('https');
-const cities = require('./db/cities');
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.get('/all-cities', (req, res) => {
-  res.json({ cities: cities });
-});
+app.get('/', (req, res) => {
+    
+    let data = [];
 
-app.post('/add-user', async (req, res) => {
-  const dataUser = new User({
-    fname: req.body.fname,
-    lname: req.body.lname,
-    email: req.body.email,
-    contact: req.body.contact,
-    address: req.body.address,
-    city: req.body.city,
-    gender: req.body.gender,
-  });
-
-  const userEmail = await User.findOne({ email: dataUser.email });
-  const userContact = await User.findOne({ contact: dataUser.contact });
-
-  if (!userEmail && !userContact) {
-    dataUser.save();
-    res.status(200).json({ msg: `User Created Successfully...` });
-  } else if (!userContact) {
-    res
-      .status(400)
-      .json({ msg: `Email "${dataUser.email}" Already Exists...` });
-  } else if (!userEmail) {
-    res
-      .status(400)
-      .json({ msg: `Contact "${dataUser.contact}" Already Exists...` });
-  } else {
-    res.status(400).json({ msg: `User Already Exists...` });
-  }
-});
-
-app.get('/all-users', async (req, res) => {
-  User.find().exec((err, users) => {
-    res.status(200).json({ usersList: users });
-  });
-});
-
-app.delete('/delete/:id', (req, res) => {
-  User.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(200).json({ msg: `User Deleted Successfully...` });
+    https.get('https://pokeapi.co/api/v2/pokemon', (response) => {
+        if(response.statusCode !== 200) {
+            console.log('Error: ' + response.statusCode);
+        }else{
+            response.on('data', (chunk) => {
+                data.push(chunk);
+            }).on('end', () => {
+                data = Buffer.concat(data).toString();
+                data = JSON.parse(data);
+                console.log("data\n",data);
+                res.json(data);
+            });
+        }
     })
-    .catch((err) => {
-      res.status(400).json({ msg: `Error Deleting User...`, error: err });
-    });
+ 
 });
 
-// get route update user
-// app.patch('/edit/:id', (req, res) => {
-//   User.findById(req.params.id, (err, user) => {
-//     if (err) {
-//       res.redirect('/list');
-//     } else {
-//       if (user == null) {
-//         res.redirect('/list');
-//       } else {
-//         res.render('edit', {
-//           userUpdate: user,
-//         });
-//       }
-//     }
-//   });
-// });
-
-// post route update user
-app.patch('/update/:id', (req, res) => {
-  User.findByIdAndUpdate(
-    req.params.id,
-    {
-      fname: req.body.fname,
-      lname: req.body.lname,
-      email: req.body.email,
-      contact: req.body.contact,
-      address: req.body.address,
-      city: req.body.city,
-      gender: req.body.gender,
-    },
-    (err) => {
-      if (err) {
-        res.status(400).json({ msg: `Error Updating User...`, error: err });
-      } else {
-        res.status(200).json({ msg: `User Updated Successfully...` });
-      }
-    }
-  );
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port http://localhost:${port}/`);
-});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
